68544688900d1a77f5be142982e75c6cf5d5c237
samuelmartinezp <samuelmartinez080525@gmail.com>
2026-07-04 15:59:04 -0500

    commit 1

---
+++ script.js
@@ -0,0 +1,48 @@
+const productos = [
+    
+    {
+       id:1,
+       nombre:"Pc Gamer Ryzen 9 9900X ",
+       categoria:"Pc",
+       precio:5000,
+       rating:5,
+       descripcion:"Equipo de alto rendimiento con potente procesador para gaming, streaming y edición profesional, ofreciendo velocidad y excelente desempeño.",
+       imagen:""
+    },
+    {
+        id:2,
+        nombre:"iPhone 17",
+        categoria:"Celulares",
+        precio:4000,
+        rating:5,
+        descripcion:"Celular con alta Potencia, cámara profesional y batería de larga duración en un diseño premium.",
+        imagen:""
+     },
+     {
+        id:3,
+        nombre:"Samsung Galaxy s26 ultra",
+        categoria:"Celulares",
+        precio:3450,
+        rating:4.5,
+        descripcion:"Es un celular con Potencia, cámara de 200 MP y Galaxy AI en un diseño premium con batería de larga duración.",
+        imagen:""
+     },
+     {
+        id:4,
+        nombre:"Hyperx Cloud III",
+        categoria:"Gaming",
+        precio:450,
+        rating:4,
+        descripcion:"Audífonos cómodos con sonido envolvente, micrófono de alta calidad y gran comodidad para largas sesiones de juego.",
+        imagen:""
+     },
+     {
+        id:5,
+        nombre:"Hyperx Cloud III",
+        categoria:"Gaming",
+        precio:450,
+        rating:4,
+        descripcion:"Audífonos cómodos con sonido envolvente, micrófono de alta calidad y gran comodidad para largas sesiones de juego.",
+        imagen:""
+     },
+]    
