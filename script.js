3ae45cb62a49276af8ddd39bb8a51b86f2f380c9
samuelmartinezp <samuelmartinez080525@gmail.com>
2026-07-04 17:42:51 -0500

    commit 2 aiudaaaaaaaaaaaaa

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const productos = [
     {
        id:1,
        nombre:"Pc Gamer Ryzen 9 9900X ",
-       categoria:"Pc",
+       categoria:"Computadoras",
        precio:5000,
        rating:5,
        descripcion:"Equipo de alto rendimiento con potente procesador para gaming, streaming y edición profesional, ofreciendo velocidad y excelente desempeño.",
-       imagen:""
+       imagen:"./img/images (6).jpeg"
     },
     {
         id:2,
@@ -16,7 +16,7 @@ const productos = [
         precio:4000,
         rating:5,
         descripcion:"Celular con alta Potencia, cámara profesional y batería de larga duración en un diseño premium.",
-        imagen:""
+        imagen:"./img/IMG-18067840_m_jpeg_1_87f249e0-b307-4c12-bd59-f4e031baed60.webp"
      },
      {
         id:3,
@@ -25,7 +25,7 @@ const productos = [
         precio:3450,
         rating:4.5,
         descripcion:"Es un celular con Potencia, cámara de 200 MP y Galaxy AI en un diseño premium con batería de larga duración.",
-        imagen:""
+        imagen:"./img/Celular-SAMSUNG-M3-512-GB-12-GB-RAM-negro-3821347_a.webp"
      },
      {
         id:4,
@@ -34,15 +34,105 @@ const productos = [
         precio:450,
         rating:4,
         descripcion:"Audífonos cómodos con sonido envolvente, micrófono de alta calidad y gran comodidad para largas sesiones de juego.",
-        imagen:""
+        imagen:"./img/545593-800-auto.webp"
      },
      {
         id:5,
-        nombre:"Hyperx Cloud III",
+        nombre:"Monitor gamer Asus",
+        categoria:"Computadoras",
+        precio:4500,
+        rating:5,
+        descripcion:"Monitor de alta frecuencia de actualización con excelente calidad de imagen y respuesta ultrarrápida para una experiencia de juego fluida.",
+        imagen:"./img/D_NQ_NP_897835-MLA99964550475_112025-O.webp"
+     },
+     {
+        id:6,
+        nombre:"Tv Kalley",
+        categoria:"Tecnologia",
+        precio:2000,
+        rating:4.5,
+        descripcion:"Monitor de alta frecuencia de actualización con excelente calidad de imagen y respuesta ultrarrápida para una experiencia de juego fluida.",
+        imagen:"./img/images (7).jpeg"
+     },
+     {
+        id:7,
+        nombre:"Teclado Mecánico Corsair Galleon",
+        categoria:"Computadoras",
+        precio:1500,
+        rating:4.9,
+        descripcion:"Teclado mecánico premium con switches de alto rendimiento, iluminación RGB y Stream Deck integrado para controlar juegos, streaming y productividad desde un solo dispositivo.",
+        imagen:"./img/450_1000.jpeg"
+     },
+     {
+        id:8,
+        nombre:"Playstation 5 + 2 Controles",
         categoria:"Gaming",
-        precio:450,
+        precio:1500,
+        rating:4.8,
+        descripcion:"Es una consola de nueva generación con gráficos 4K, carga rápida y alto rendimiento.",
+        imagen:"./img/consola-playstation-5-1-tb-slim-ps5-2-controles-blancos-1028456760.webp"
+     },
+     {
+        id:9,
+        nombre:"Silla gamer",
+        categoria:"Gaming",
+        precio:300,
+        rating:4.2,
+        descripcion:"silla ergonómica diseñada para videojuegos, con soporte para espalda y cuello, altura ajustable, apoyabrazos y materiales cómodos para largas sesiones de uso.",
+        imagen:"./img/lknlkkn.png"
+     },
+     {
+        id:10,
+        nombre:"Camara Web Lenovo",
+        categoria:"Gaming",
+        precio:150,
+        rating:5,
+        descripcion:"sirve para streaming y transmisiones en vivo, buena nitidez y micrófono integrado. Es una opción básica para empezar a hacer directos en Twitch o YouTube sin complicaciones.",
+        imagen:"./img/images (8).jpeg"
+     },
+     {
+        id:11,
+        nombre:"Mouse Gamer Redragon",
+        categoria:"Computadoras",
+        precio:200,
+        rating:4.9,
+        descripcion:"es un ratón diseñado para videojuegos, con sensor de alta precisión, botones programables e iluminación RGB, ideal para mejorar la velocidad y control en juegos competitivos.",
+        imagen:"./img/images (9).jpeg"
+     },
+     {
+        id:12,
+        nombre:"Escritorio Gamer",
+        categoria:"Gaming",
+        precio:100,
         rating:4,
-        descripcion:"Audífonos cómodos con sonido envolvente, micrófono de alta calidad y gran comodidad para largas sesiones de juego.",
-        imagen:""
+        descripcion:"mesa diseñada para videojuegos, con superficie amplia, estructura resistente, espacio para monitor, teclado y accesorios, y en algunos casos gestión de cables e iluminación RGB.",
+        imagen:"./img/157766-800-800.png"
+     },
+     {
+        id:13,
+        nombre:"Set de Parlantes",
+        categoria:"Tecnologia",
+        precio:250,
+        rating:5,
+        descripcion:"sistema de audio para PC o consola que ofrece sonido potente y envolvente, con buen bajo, iluminación RGB en algunos modelos y conexión por USB o Bluetooth para mejorar la experiencia de juego.",
+        imagen:"./img/images (10).jpeg"
+     },
+     {
+        id:14,
+        nombre:"Barras led RGB",
+        categoria:"Tecnologia",
+        precio:50,
+        rating:4,
+        descripcion:"tiras o focos de iluminación con colores personalizables que se usan para ambientar el setup, crear efectos visuales y mejorar la estética del escritorio o habitación gaming.",
+        imagen:"./img/28914655-800-auto.webp"
+     },
+     {
+        id:15,
+        nombre:"Celular Xiaomi Redmi",
+        categoria:"Celulares",
+        precio:3500,
+        rating:5,
+        descripcion:"es un celular de gama accesible que ofrece buena batería, pantalla de calidad y rendimiento sólido para uso diario, redes sociales y juegos ligeros, a un precio económico.",
+        imagen:"./img/Celular-XIAOMI-Redmi-15-8256GB-Morado-256-GB-8-GB-RAM-3746127_a.webp"
      },
 ]    
